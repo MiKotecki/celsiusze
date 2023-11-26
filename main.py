0aecd738890d8cc8fb4409c07729b26353ac8a52
Mikolaj Kotecki <mikolaj9610@gmail.com>
2023-11-26 22:10:33 +0100

    na kelwiny oraz fahrenheity

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+def celsius_to_fahrenheit(temperature):
+    return temperature * 1.8 + 32
+
+
 def celsius_to_kelvin(temperature):
     return temperature + 273.15
 
-if __name__ == '__main__':
-    with open("input.txt", "r") as file, open("result.txt", "w") as result:
-        lines = file.readlines()
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as data, open("result.txt", "w") as result:
+        lines = data.readlines()
         temperatures = []
         for line in lines:
-            temperature = float(line.strip())
+            temperature = round(float(line.strip()), 2)
             temperatures.append(temperature)
-        print(lines)
-        print(temperatures)
-        result.write("Celsius; Kelvin\n")
+        result.write("Celsius; Kelvin; Fahrenheit\n")
         for temperature in temperatures:
-            result.write(f"{temperature}; {celsius_to_kelvin(temperature)}\n")
+            result.write(
+                f"{temperature}; {round(celsius_to_kelvin(temperature), 2)}; {round(celsius_to_fahrenheit(temperature), 2)}\n")
